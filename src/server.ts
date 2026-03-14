08ba65aeebc1aa9f5001f475c142628a22c9e1e2
shakiburrashid <shakiburrashid7@gmail.com>
2026-03-14 19:25:28 +0600

    done all

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express'
-import vehicleCall from './src/Config/Database.js';
-import router from './src/Module/User/user.route.js';
-import config from './src/Config/ConfigEnv.js';
-import { vehicle_route } from './src/Module/Vehicle/vehicle.route.js';
-import { booking_router } from './src/Module/Booking/booking.route.js';
+import vehicleCall from './Config/Database.js';
+import router from './Module/User/user.route.js';
+import config from './Config/ConfigEnv.js';
+import { vehicle_route } from './Module/Vehicle/vehicle.route.js';
+import { booking_router } from './Module/Booking/booking.route.js';
 
 
 
